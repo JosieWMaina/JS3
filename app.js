14cd82fcd5750ee00ae2ad16dc594b490bf789c0
JosieWMaina <josytyra@yahoo.com>
2022-01-17 11:33:18 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+
+/* =========================================================== */
+/* DROPDOWN MENU */
+/* =========================================================== */
+
+function toggleFunc() {
+  document.getElementById("dropdown-menu").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+/* =========================================================== */
+/* MODAL */
+/* =========================================================== */
+
+var modal = document.getElementById("custom-modal");
+var btn = document.getElementById("modal-btn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
